227b886c26845dd5ea0891cd59c059a985ad3163
Grant King <grantryan.king@gmail.com>
2026-03-15 15:16:08 -0700

    Update Introduction.vue
    Add a special formatting section to Baum's signature, grouping the location info into its own thing without whitespace between.
    
    Also I formatted the whole .vue file with the built-in formatter in VSCode.

--- vue-project/src/components/Introduction.vue
+++ vue-project/src/components/Introduction.vue
@@ -1,38 +1,62 @@
 <template>
 
     <div>
-Prologue
-</div>
-<div>
-Through the kindness of Dorothy Gale of Kansas, afterward Princess Dorothy of Oz, an humble writer in the United States of America was once appointed Royal Historian of Oz, with the privilege of writing the chronicle of that wonderful fairyland. But after making six books about the adventures of those interesting but queer people who live in the Land of Oz, the Historian learned with sorrow that by an edict of the Supreme Ruler, Ozma of Oz, her country would thereafter be rendered invisible to all who lived outside its borders and that all communication with Oz would, in the future, be cut off.
-</div>
-<div>
-The children who had learned to look for the books about Oz and who loved the stories about the gay and happy people inhabiting that favored country, were as sorry as their Historian that there would be no more books of Oz stories. They wrote many letters asking if the Historian did not know of some adventures to write about that had happened before the Land of Oz was shut out from all the rest of the world. But he did not know of any. Finally one of the children inquired why we couldn't hear from Princess Dorothy by wireless telegraph, which would enable her to communicate to the Historian whatever happened in the far-off Land of Oz without his seeing her, or even knowing just where Oz is.
-</div>
-<div>
-That seemed a good idea; so the Historian rigged up a high tower in his back yard, and took lessons in wireless telegraphy until he understood it, and then began to call "Princess Dorothy of Oz" by sending messages into the air.
-</div>
-<div>
-Now, it wasn't likely that Dorothy would be looking for wireless messages or would heed the call; but one thing the Historian was sure of, and that was that the powerful Sorceress, Glinda, would know what he was doing and that he desired to communicate with Dorothy. For Glinda has a big book in which is recorded every event that takes place anywhere in the world, just the moment that it happens, and so of course the book would tell her about the wireless message.
-</div>
-<div>
-And that was the way Dorothy heard that the Historian wanted to speak with her, and there was a Shaggy Man in the Land of Oz who knew how to telegraph a wireless reply. The result was that the Historian begged so hard to be told the latest news of Oz, so that he could write it down for the children to read, that Dorothy asked permission of Ozma and Ozma graciously consented.
-</div>
-<div>
-That is why, after two long years of waiting, another Oz story is now presented to the children of America. This would not have been possible had not some clever man invented the "wireless" and an equally clever child suggested the idea of reaching the mysterious Land of Oz by its means.
-</div>
-<div>
-L. Frank Baum.
-</div>
-<div>
-"OZCOT"
-</div>
-<div>
-     at Hollywood
-</div>
-<div>
-         in California
-</div>
+        Prologue
+    </div>
+    <div>
+        Through the kindness of Dorothy Gale of Kansas, afterward Princess Dorothy of Oz, an humble writer in the United
+        States of America was once appointed Royal Historian of Oz, with the privilege of writing the chronicle of that
+        wonderful fairyland. But after making six books about the adventures of those interesting but queer people who
+        live in the Land of Oz, the Historian learned with sorrow that by an edict of the Supreme Ruler, Ozma of Oz, her
+        country would thereafter be rendered invisible to all who lived outside its borders and that all communication
+        with Oz would, in the future, be cut off.
+    </div>
+    <div>
+        The children who had learned to look for the books about Oz and who loved the stories about the gay and happy
+        people inhabiting that favored country, were as sorry as their Historian that there would be no more books of Oz
+        stories. They wrote many letters asking if the Historian did not know of some adventures to write about that had
+        happened before the Land of Oz was shut out from all the rest of the world. But he did not know of any. Finally
+        one of the children inquired why we couldn't hear from Princess Dorothy by wireless telegraph, which would
+        enable her to communicate to the Historian whatever happened in the far-off Land of Oz without his seeing her,
+        or even knowing just where Oz is.
+    </div>
+    <div>
+        That seemed a good idea; so the Historian rigged up a high tower in his back yard, and took lessons in wireless
+        telegraphy until he understood it, and then began to call "Princess Dorothy of Oz" by sending messages into the
+        air.
+    </div>
+    <div>
+        Now, it wasn't likely that Dorothy would be looking for wireless messages or would heed the call; but one thing
+        the Historian was sure of, and that was that the powerful Sorceress, Glinda, would know what he was doing and
+        that he desired to communicate with Dorothy. For Glinda has a big book in which is recorded every event that
+        takes place anywhere in the world, just the moment that it happens, and so of course the book would tell her
+        about the wireless message.
+    </div>
+    <div>
+        And that was the way Dorothy heard that the Historian wanted to speak with her, and there was a Shaggy Man in
+        the Land of Oz who knew how to telegraph a wireless reply. The result was that the Historian begged so hard to
+        be told the latest news of Oz, so that he could write it down for the children to read, that Dorothy asked
+        permission of Ozma and Ozma graciously consented.
+    </div>
+    <div>
+        That is why, after two long years of waiting, another Oz story is now presented to the children of America. This
+        would not have been possible had not some clever man invented the "wireless" and an equally clever child
+        suggested the idea of reaching the mysterious Land of Oz by its means.
+    </div>
+    <div>
+        L. Frank Baum.
+    </div>
+    <div>
+        <div>
+            "OZCOT"
+        </div>
+        <div>
+            at Hollywood
+        </div>
+        <div>
+            in California
+        </div>
+    </div>
 
 
     <div class="h-32"></div>
@@ -41,11 +65,11 @@ L. Frank Baum.
             <div
                 class="grid grid-cols-1 gap-2 text-xl font-semibold text-center text-white font-serif bg-black/40 rounded py-16 border-16 border-double border-white/80">
                 <div>
-Affectionately Dedicated to my young friend
-</div>
-<div>
-Sumner Hamilton Britton of Chicago
-</div>
+                    Affectionately Dedicated to my young friend
+                </div>
+                <div>
+                    Sumner Hamilton Britton of Chicago
+                </div>
 
             </div>
         </div>
