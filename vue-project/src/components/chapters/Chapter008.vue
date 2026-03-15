09f249b00ecc852e9664be6ebafa74c2599668fc
Grant King <grantryan.king@gmail.com>
2026-03-15 15:43:44 -0700

    Update Chapter008.vue
    Add special formatting sections for the owl's poems.
    
    Also format the whole .vue file.

--- vue-project/src/components/chapters/Chapter008.vue
+++ vue-project/src/components/chapters/Chapter008.vue
@@ -8,209 +8,242 @@
     </div>
     <div class="h-16"></div>
     <div>
-On they went, and half an hour's steady walking brought them to a house somewhat better than the two they had already passed. It stood close to the roadside and over the door was a sign that read: "Miss Foolish Owl and Mr. Wise Donkey: Public Advisers."
-</div>
-<div>
-When Ojo read this sign aloud Scraps said laughingly: "Well, here is a place to get all the advice we want, maybe more than we need. Let's go in."
-</div>
-<div>
-The boy knocked at the door.
-</div>
-<div>
-"Come in!" called a deep bass voice.
-</div>
-<div>
-So they opened the door and entered the house, where a little light-brown donkey, dressed in a blue apron and a blue cap, was engaged in dusting the furniture with a blue cloth. On a shelf over the window sat a great blue owl with a blue sunbonnet on her head, blinking her big round eyes at the visitors.
-</div>
-<div>
-"Good morning," said the donkey, in his deep voice, which seemed bigger than he was. "Did you come to us for advice?"
-</div>
-<div>
-"Why, we came, anyhow," replied Scraps, "and now we are here we may as well have some advice. It's free, isn't it?"
-</div>
-<div>
-"Certainly," said the donkey. "Advice doesn't cost anything—unless you follow it. Permit me to say, by the way, that you are the queerest lot of travelers that ever came to my shop. Judging you merely by appearances, I think you'd better talk to the Foolish Owl yonder."
-</div>
-<div>
-They turned to look at the bird, which fluttered its wings and stared back at them with its big eyes.
-</div>
-<div>
-"Hoot-ti-toot-ti-toot!" cried the owl.
-</div>
-<div>
-"Fiddle-cum-foo,
-</div>
-<div>
-Howdy-do?
-</div>
-<div>
-Riddle-cum, tiddle-cum,
-</div>
-<div>
-Too-ra-la-loo!"
-</div>
-<div>
-"That beats your poetry, Scraps," said Ojo.
-</div>
-<div>
-"It's just nonsense!" declared the Glass Cat.
-</div>
-<div>
-"But it's good advice for the foolish," said the donkey, admiringly. "Listen to my partner, and you can't go wrong."
-</div>
-<div>
-Said the owl in a grumbling voice:
-</div>
-<div>
-"Patchwork Girl has come to life;
-</div>
-<div>
-No one's sweetheart, no one's wife;
-</div>
-<div>
-Lacking sense and loving fun,
-</div>
-<div>
-She'll be snubbed by everyone."
-</div>
-<div>
-"Quite a compliment! Quite a compliment, I declare," exclaimed the donkey, turning to look at Scraps. "You are certainly a wonder, my dear, and I fancy you'd make a splendid pincushion. If you belonged to me, I'd wear smoked glasses when I looked at you."
-</div>
-<div>
-"Why?" asked the Patchwork Girl.
-</div>
-<div>
-"Because you are so gay and gaudy."
-</div>
-<div>
-"It is my beauty that dazzles you," she asserted. "You Munchkin people all strut around in your stupid blue color, while I—"
-</div>
-<div>
-"You are wrong in calling me a Munchkin," interrupted the donkey, "for I was born in the Land of Mo and came to visit the Land of Oz on the day it was shut off from all the rest of the world. So here I am obliged to stay, and I confess it is a very pleasant country to live in."
-</div>
-<div>
-"Hoot-ti-toot!" cried the owl;
-</div>
-<div>
-"Ojo's searching for a charm,
-</div>
-<div>
-'Cause Unc Nunkie's come to harm.
-</div>
-<div>
-Charms are scarce; they're hard to get;
-</div>
-<div>
-Ojo's got a job, you bet!"
-</div>
-<div>
-"Is the owl so very foolish?" asked the boy.
-</div>
-<div>
-"Extremely so," replied the donkey. "Notice what vulgar expressions she uses. But I admire the owl for the reason that she is positively foolish. Owls are supposed to be so very wise, generally, that a foolish one is unusual, and you perhaps know that anything or anyone unusual is sure to be interesting to the wise."
-</div>
-<div>
-The owl flapped its wings again, muttering these words:
-</div>
-<div>
-"It's hard to be a glassy cat—
-</div>
-<div>
-No cat can be more hard than that;
-</div>
-<div>
-She's so transparent, every act
-</div>
-<div>
-Is clear to us, and that's a fact."
-</div>
-<div>
-"Have you noticed my pink brains?" inquired Bungle, proudly. "You can see 'em work."
-</div>
-<div>
-"Not in the daytime," said the donkey. "She can't see very well by day, poor thing. But her advice is excellent. I advise you all to follow it."
-</div>
-<div>
-"The owl hasn't given us any advice, as yet," the boy declared.
-</div>
-<div>
-"No? Then what do you call all those sweet poems?"
-</div>
-<div>
-"Just foolishness," replied Ojo. "Scraps does the same thing."
-</div>
-<div>
-"Foolishness! Of course! To be sure! The Foolish Owl must be foolish or she wouldn't be the Foolish Owl. You are very complimentary to my partner, indeed," asserted the donkey, rubbing his front hoofs together as if highly pleased.
-</div>
-<div>
-"The sign says that you are wise," remarked Scraps to the donkey. "I wish you would prove it."
-</div>
-<div>
-"With great pleasure," returned the beast. "Put me to the test, my dear Patches, and I'll prove my wisdom in the wink of an eye."
-</div>
-<div>
-"What is the best way to get to the Emerald City?" asked Ojo.
-</div>
-<div>
-"Walk," said the donkey.
-</div>
-<div>
-"I know; but what road shall I take?" was the boy's next question.
-</div>
-<div>
-"The road of yellow bricks, of course. It leads directly to the Emerald City."
-</div>
-<div>
-"And how shall we find the road of yellow bricks?"
-</div>
-<div>
-"By keeping along the path you have been following. You'll come to the yellow bricks pretty soon, and you'll know them when you see them because they're the only yellow things in the blue country."
-</div>
-<div>
-"Thank you," said the boy. "At last you have told me something."
-</div>
-<div>
-"Is that the extent of your wisdom?" asked Scraps.
-</div>
-<div>
-"No," replied the donkey; "I know many other things, but they wouldn't interest you. So I'll give you a last word of advice: move on, for the sooner you do that the sooner you'll get to the Emerald City of Oz."
-</div>
-<div>
-"Hoot-ti-toot-ti-toot-ti-too!" screeched the owl;
-</div>
-<div>
-"Off you go! fast or slow,
-</div>
-<div>
-Where you're going you don't know.
-</div>
-<div>
-Patches, Bungle, Munchkin lad,
-</div>
-<div>
-Facing fortunes good and bad,
-</div>
-<div>
-Meeting dangers grave and sad,
-</div>
-<div>
-Sometimes worried, sometimes glad—
-</div>
-<div>
-Where you're going you don't know,
-</div>
-<div>
-Nor do I, but off you go!"
-</div>
-<div>
-"Sounds like a hint, to me," said the Patchwork Girl.
-</div>
-<div>
-"Then let's take it and go," replied Ojo.
-</div>
-<div>
-They said good-bye to the Wise Donkey and the Foolish Owl and at once resumed their journey.
-</div>
+        On they went, and half an hour's steady walking brought them to a house somewhat better than the two they had
+        already passed. It stood close to the roadside and over the door was a sign that read: "Miss Foolish Owl and Mr.
+        Wise Donkey: Public Advisers."
+    </div>
+    <div>
+        When Ojo read this sign aloud Scraps said laughingly: "Well, here is a place to get all the advice we want,
+        maybe more than we need. Let's go in."
+    </div>
+    <div>
+        The boy knocked at the door.
+    </div>
+    <div>
+        "Come in!" called a deep bass voice.
+    </div>
+    <div>
+        So they opened the door and entered the house, where a little light-brown donkey, dressed in a blue apron and a
+        blue cap, was engaged in dusting the furniture with a blue cloth. On a shelf over the window sat a great blue
+        owl with a blue sunbonnet on her head, blinking her big round eyes at the visitors.
+    </div>
+    <div>
+        "Good morning," said the donkey, in his deep voice, which seemed bigger than he was. "Did you come to us for
+        advice?"
+    </div>
+    <div>
+        "Why, we came, anyhow," replied Scraps, "and now we are here we may as well have some advice. It's free, isn't
+        it?"
+    </div>
+    <div>
+        "Certainly," said the donkey. "Advice doesn't cost anything—unless you follow it. Permit me to say, by the way,
+        that you are the queerest lot of travelers that ever came to my shop. Judging you merely by appearances, I think
+        you'd better talk to the Foolish Owl yonder."
+    </div>
+    <div>
+        They turned to look at the bird, which fluttered its wings and stared back at them with its big eyes.
+    </div>
+    <div>
+        "Hoot-ti-toot-ti-toot!" cried the owl.
+    </div>
+    <div>
+        <div>
+            "Fiddle-cum-foo,
+        </div>
+        <div>
+            Howdy-do?
+        </div>
+        <div>
+            Riddle-cum, tiddle-cum,
+        </div>
+        <div>
+            Too-ra-la-loo!"
+        </div>
+    </div>
+    <div>
+        "That beats your poetry, Scraps," said Ojo.
+    </div>
+    <div>
+        "It's just nonsense!" declared the Glass Cat.
+    </div>
+    <div>
+        "But it's good advice for the foolish," said the donkey, admiringly. "Listen to my partner, and you can't go
+        wrong."
+    </div>
+    <div>
+        Said the owl in a grumbling voice:
+    </div>
+    <div>
+        <div>
+            "Patchwork Girl has come to life;
+        </div>
+        <div>
+            No one's sweetheart, no one's wife;
+        </div>
+        <div>
+            Lacking sense and loving fun,
+        </div>
+        <div>
+            She'll be snubbed by everyone."
+        </div>
+    </div>
+    <div>
+        "Quite a compliment! Quite a compliment, I declare," exclaimed the donkey, turning to look at Scraps. "You are
+        certainly a wonder, my dear, and I fancy you'd make a splendid pincushion. If you belonged to me, I'd wear
+        smoked glasses when I looked at you."
+    </div>
+    <div>
+        "Why?" asked the Patchwork Girl.
+    </div>
+    <div>
+        "Because you are so gay and gaudy."
+    </div>
+    <div>
+        "It is my beauty that dazzles you," she asserted. "You Munchkin people all strut around in your stupid blue
+        color, while I—"
+    </div>
+    <div>
+        "You are wrong in calling me a Munchkin," interrupted the donkey, "for I was born in the Land of Mo and came to
+        visit the Land of Oz on the day it was shut off from all the rest of the world. So here I am obliged to stay,
+        and I confess it is a very pleasant country to live in."
+    </div>
+    <div>
+        "Hoot-ti-toot!" cried the owl;
+    </div>
+    <div>
+        <div>
+            "Ojo's searching for a charm,
+        </div>
+        <div>
+            'Cause Unc Nunkie's come to harm.
+        </div>
+        <div>
+            Charms are scarce; they're hard to get;
+        </div>
+        <div>
+            Ojo's got a job, you bet!"
+        </div>
+    </div>
+    <div>
+        "Is the owl so very foolish?" asked the boy.
+    </div>
+    <div>
+        "Extremely so," replied the donkey. "Notice what vulgar expressions she uses. But I admire the owl for the
+        reason that she is positively foolish. Owls are supposed to be so very wise, generally, that a foolish one is
+        unusual, and you perhaps know that anything or anyone unusual is sure to be interesting to the wise."
+    </div>
+    <div>
+        The owl flapped its wings again, muttering these words:
+    </div>
+    <div>
+        <div>
+            "It's hard to be a glassy cat—
+        </div>
+        <div>
+            No cat can be more hard than that;
+        </div>
+        <div>
+            She's so transparent, every act
+        </div>
+        <div>
+            Is clear to us, and that's a fact."
+        </div>
+    </div>
+    <div>
+        "Have you noticed my pink brains?" inquired Bungle, proudly. "You can see 'em work."
+    </div>
+    <div>
+        "Not in the daytime," said the donkey. "She can't see very well by day, poor thing. But her advice is excellent.
+        I advise you all to follow it."
+    </div>
+    <div>
+        "The owl hasn't given us any advice, as yet," the boy declared.
+    </div>
+    <div>
+        "No? Then what do you call all those sweet poems?"
+    </div>
+    <div>
+        "Just foolishness," replied Ojo. "Scraps does the same thing."
+    </div>
+    <div>
+        "Foolishness! Of course! To be sure! The Foolish Owl must be foolish or she wouldn't be the Foolish Owl. You are
+        very complimentary to my partner, indeed," asserted the donkey, rubbing his front hoofs together as if highly
+        pleased.
+    </div>
+    <div>
+        "The sign says that you are wise," remarked Scraps to the donkey. "I wish you would prove it."
+    </div>
+    <div>
+        "With great pleasure," returned the beast. "Put me to the test, my dear Patches, and I'll prove my wisdom in the
+        wink of an eye."
+    </div>
+    <div>
+        "What is the best way to get to the Emerald City?" asked Ojo.
+    </div>
+    <div>
+        "Walk," said the donkey.
+    </div>
+    <div>
+        "I know; but what road shall I take?" was the boy's next question.
+    </div>
+    <div>
+        "The road of yellow bricks, of course. It leads directly to the Emerald City."
+    </div>
+    <div>
+        "And how shall we find the road of yellow bricks?"
+    </div>
+    <div>
+        "By keeping along the path you have been following. You'll come to the yellow bricks pretty soon, and you'll
+        know them when you see them because they're the only yellow things in the blue country."
+    </div>
+    <div>
+        "Thank you," said the boy. "At last you have told me something."
+    </div>
+    <div>
+        "Is that the extent of your wisdom?" asked Scraps.
+    </div>
+    <div>
+        "No," replied the donkey; "I know many other things, but they wouldn't interest you. So I'll give you a last
+        word of advice: move on, for the sooner you do that the sooner you'll get to the Emerald City of Oz."
+    </div>
+    <div>
+        "Hoot-ti-toot-ti-toot-ti-too!" screeched the owl;
+    </div>
+    <div>
+        <div>
+            "Off you go! fast or slow,
+        </div>
+        <div>
+            Where you're going you don't know.
+        </div>
+        <div>
+            Patches, Bungle, Munchkin lad,
+        </div>
+        <div>
+            Facing fortunes good and bad,
+        </div>
+        <div>
+            Meeting dangers grave and sad,
+        </div>
+        <div>
+            Sometimes worried, sometimes glad—
+        </div>
+        <div>
+            Where you're going you don't know,
+        </div>
+        <div>
+            Nor do I, but off you go!"
+        </div>
+    </div>
+    <div>
+        "Sounds like a hint, to me," said the Patchwork Girl.
+    </div>
+    <div>
+        "Then let's take it and go," replied Ojo.
+    </div>
+    <div>
+        They said good-bye to the Wise Donkey and the Foolish Owl and at once resumed their journey.
+    </div>
 
 </template>
 
